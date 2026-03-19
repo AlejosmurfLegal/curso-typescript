33dbce18e7c8cb711b7ddca70e33bf4f1da61a3b
AlejosmurfLegal <manuel.vega@legalsuitelatam.com>
2026-03-19 09:41:31 -0500

    Listas, Interfaces y Tuplas

--- Listas/listas.js
+++ Listas/listas.js
@@ -4,3 +4,42 @@ var edades = [25, 30, 22, 28];
 console.log("Edades: ", edades);
 var mixto = ["Manuel", 25, true];
 console.log("Mixto: ", mixto);
+var personas = [
+    { nombre: "Carlos", edad: 35, desarrollador: true },
+    { nombre: "Lucía", edad: 28, desarrollador: false },
+    { nombre: "Sofía", edad: 22, desarrollador: true }
+];
+console.log("Personas: ", personas);
+personas.push({ nombre: "Miguel", edad: 30, desarrollador: false });
+console.log("Personas Actualizadas: ", personas);
+// Tuplas
+var personaTupla;
+personaTupla = ["Laura", 27, true];
+console.log("Persona Tupla: ", personaTupla);
+var personasTupla = [];
+personasTupla.push(["Andrés", 32, true]);
+personasTupla.push(["Manuel", 25, true]);
+personasTupla.push(["Camilo", 40, false]);
+personasTupla.forEach(function (persona) {
+    console.log("Persona en Tupla: ", persona);
+    var nombre = persona[0];
+    var edad = persona[1];
+    var desarrollador = persona[2];
+    console.log("Nombre: ", nombre);
+    console.log("Edad: ", edad);
+    console.log("Desarrollador: ", desarrollador);
+});
+//Enumeradores
+var DiaDeLaSemana;
+(function (DiaDeLaSemana) {
+    DiaDeLaSemana[DiaDeLaSemana["Lunes"] = 0] = "Lunes";
+    DiaDeLaSemana[DiaDeLaSemana["Martes"] = 1] = "Martes";
+    DiaDeLaSemana[DiaDeLaSemana["Mi\u00E9rcoles"] = 2] = "Mi\u00E9rcoles";
+    DiaDeLaSemana[DiaDeLaSemana["Jueves"] = 3] = "Jueves";
+    DiaDeLaSemana[DiaDeLaSemana["Viernes"] = 4] = "Viernes";
+    DiaDeLaSemana[DiaDeLaSemana["S\u00E1bado"] = 5] = "S\u00E1bado";
+    DiaDeLaSemana[DiaDeLaSemana["Domingo"] = 6] = "Domingo";
+})(DiaDeLaSemana || (DiaDeLaSemana = {}));
+var dia = DiaDeLaSemana.Miércoles;
+console.log("Día de la Semana: ", dia);
+console.log("Día de la Semana (Nombre): ", DiaDeLaSemana[dia]);
--- Listas/listas.ts
+++ Listas/listas.ts
@@ -6,3 +6,61 @@ console.log("Edades: ", edades);
 
 let mixto: any[] = ["Manuel", 25, true];
 console.log("Mixto: ", mixto);
+
+//Interface
+interface Persona {
+    nombre: string;
+    edad: number;
+    desarrollador: boolean;
+}
+
+let personas:Persona[] = [
+    {nombre: "Carlos", edad: 35, desarrollador: true},
+    {nombre: "Lucía", edad: 28, desarrollador: false},
+    {nombre: "Sofía", edad: 22, desarrollador: true}
+];
+
+console.log("Personas: ", personas);
+
+personas.push({nombre: "Miguel", edad: 30, desarrollador: false});
+
+console.log("Personas Actualizadas: ", personas);
+
+// Tuplas
+let personaTupla: [string, number, boolean];
+personaTupla = ["Laura", 27, true];
+
+console.log("Persona Tupla: ", personaTupla);
+
+let personasTupla: [string, number, boolean][] = [];
+
+personasTupla.push(["Andrés", 32, true]);
+personasTupla.push(["Manuel", 25, true]);
+personasTupla.push(["Camilo", 40, false]);
+
+personasTupla.forEach(persona => {
+    console.log("Persona en Tupla: ", persona);
+
+    let nombre: string = persona[0];
+    let edad: number = persona[1];
+    let desarrollador: boolean = persona[2];
+
+    console.log("Nombre: ", nombre);
+    console.log("Edad: ", edad);
+    console.log("Desarrollador: ", desarrollador);
+});
+
+//Enumeradores
+enum DiaDeLaSemana {
+    Lunes,
+    Martes,
+    Miércoles,
+    Jueves,
+    Viernes,
+    Sábado,
+    Domingo
+}
+
+let dia: DiaDeLaSemana = DiaDeLaSemana.Miércoles;
+console.log("Día de la Semana: ", dia);
+console.log("Día de la Semana (Nombre): ", DiaDeLaSemana[dia]);
