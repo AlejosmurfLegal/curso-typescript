f7c22fdf9173a9afffee2c50fc5a31c95ad8e068
AlejosmurfLegal <manuel.vega@legalsuitelatam.com>
2026-04-01 12:05:58 -0500

    Se agrega parte faltante

--- Interfaces/interface2.ts
+++ Interfaces/interface2.ts
@@ -7,6 +7,38 @@ interface Direccion {
 let direccion: Direccion = {
   calle: "Calle Principal",
   numero: 123,
+  ciudad: "Ciudad Ejemplo"
 };
 
 console.log("Dirección: ", direccion);
+
+//Readonly Properties
+interface Persona {
+  readonly nombre: string;
+  readonly edad: number;
+  readonly esDesarrollador: boolean;
+}
+
+let persona: Persona = {
+  nombre: "Manuel",
+  edad: 25,
+  esDesarrollador: true,
+}
+
+console.log("Persona: ", persona);
+
+// persona.nombre = "Ana"; // Error: Cannot assign to 'nombre' because it is a read-only property.
+
+//Interfaz extendida
+interface Empleado extends Persona {
+  readonly puesto: string;
+}
+
+let empleadoRead: Empleado = {
+  nombre: "Manuel",
+  edad: 25,
+  esDesarrollador: true,
+  puesto: "Desarrollador frontend"
+}
+
+console.log("Empleado: ", empleadoRead);
