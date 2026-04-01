12bc28bcc266f4164bbe77523d16b71079648166
AlejosmurfLegal <manuel.vega@legalsuitelatam.com>
2026-04-01 13:40:07 -0500

    Clase 10, clases, contructores y metodos, implementacion con export y import

---
+++ Clases/implementacion.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var clase_1 = require("./clase");
+var persona = new clase_1.Persona("Manuel", 25, true);
+console.log(persona);
+console.log(persona.saludar());
