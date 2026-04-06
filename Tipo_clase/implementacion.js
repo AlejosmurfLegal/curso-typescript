64adab113524719f75a2cc87ba213c5739ffd58d
AlejosmurfLegal <manuel.vega@legalsuitelatam.com>
2026-04-06 09:58:37 -0500

    Clase12

--- Tipo_clase/implementacion.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var clase_1 = require("./clase");
-var persona = new clase_1.Persona("Manuel", 25, true);
-console.log(persona);
-console.log(persona.saludar());
